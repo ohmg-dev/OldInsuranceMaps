9ab38671a3b01b748e86a08da1eb31abcdfedb8e
Adam Cox <mr.adamcox@gmail.com>
2022-01-07 15:28:25 -0600

    update front page to show volumes as progress count

--- loc_insurancemaps/context_processors.py
+++ loc_insurancemaps/context_processors.py
@@ -5,7 +5,10 @@ from .models import Volume, get_volume
 def loc_info(request):
 
     info = {
-        "volume_ct": Volume.objects.exclude(loaded_by=None).count(),
+        "volume_ct": {
+            "total": Volume.objects.all().count(),
+            "started": Volume.objects.exclude(loaded_by=None).count(),
+        }
     }
 
     resource_type, res_id = analyze_url(request)
