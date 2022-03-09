c2052328b35f3bd23326ee8a801c64b7d85f51d3
Adam Cox <mr.adamcox@gmail.com>
2022-03-09 15:05:07 -0600

    initial creation of migration command for legacy sessions

---
+++ georeference/management/commands/oneoffs.py
@@ -0,0 +1,111 @@
+import json
+
+from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
+from django.utils import timezone
+
+from georeference.proxy_models import DocumentProxy, LayerProxy
+from georeference.utils import TKeywordManager
+from georeference.splitter import Splitter
+
+class Command(BaseCommand):
+    help = 'Command line access point for the internal georeferencing utilities.'
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "operation",
+            choices=[
+                "migrate-sessions",
+            ],
+            help="specify the operation to carry out",
+        )
+        parser.add_argument(
+            "--clean",
+            action="store_true",
+            help="remove all instances of new sessions before migrating old ones",
+        )
+
+    def handle(self, *args, **options):
+
+        if options["operation"] == "migrate-sessions":
+            tkm = TKeywordManager()
+            try:
+                from georeference.models import (
+                    SplitEvaluation,
+                    GeoreferenceSession,
+                    MaskSession,
+                )
+            except ImportError:
+                exit()
+            from georeference.models import (
+                PrepSession,
+                GeorefSession,
+                TrimSession,
+            )
+            if options['clean'] is True:
+                PrepSession.objects.all().delete()
+                GeorefSession.objects.all().delete()
+                TrimSession.objects.all().delete()
+            for se in SplitEvaluation.objects.all():
+                with transaction.atomic():
+                    try:
+                        ps = PrepSession.objects.create(
+                            document=se.document,
+                            user=se.user,
+                            date_created=se.created,
+                            date_run=se.created,
+                        )
+                        ps.data["split_needed"] = se.split_needed
+
+                        if se.cutlines is None or len(se.cutlines) == 0:
+                            ps.data["cutlines"] = []
+                            ps.data["divisions"] = []
+                        else:
+                            ps.data["cutlines"] = se.cutlines
+                            s = Splitter(image_file=ps.document.doc_file.path)
+                            ps.data['divisions'] = s.generate_divisions(se.cutlines)
+
+                        if tkm.get_status(ps.document) == "splitting":
+                            ps.stage = "processing"
+                        else:
+                            ps.stage = "success"
+                        ps.status_note = f"(migrated on {timezone.now()})"
+                        ps.save()
+                    except Exception as e:
+                        print(f"error migrating: SplitEvaluation {se.pk}")
+                        raise e
+            for grs in GeoreferenceSession.objects.all():
+                with transaction.atomic():
+                    try:
+                        gs = GeorefSession.objects.create(
+                            document=grs.document,
+                            layer=grs.layer,
+                            user=grs.user,
+                            date_created=grs.created,
+                            date_run=grs.created,
+                        )
+                        gs.data["gcps"] = grs.gcps_used
+                        gs.data["epsg"] = grs.crs_epsg_used
+                        gs.data["transformation"] = grs.transformation_used
+                        gs.status_note = f"(migrated on {timezone.now()})"
+                        gs.save()
+                    except Exception as e:
+                        print(f"error migrating: GeoreferenceSession {grs.pk}")
+                        raise e
+            for ms in MaskSession.objects.all():
+                with transaction.atomic():
+                    try:
+                        ts = TrimSession.objects.create(
+                            layer=ms.layer,
+                            user=grs.user,
+                            date_created=grs.created,
+                            date_run=grs.created,
+                        )
+                        if ms.polygon is not None:
+                            ts.data["polygon"] = json.loads(ms.polygon.geojson)
+                        else:
+                            ts.data["polygon"] = {}
+                        ts.status_note = f"(migrated on {timezone.now()})"
+                        ts.save()
+                    except Exception as e:
+                        print(f"error migrating: MaskSession {ms.pk}")
+                        raise e
