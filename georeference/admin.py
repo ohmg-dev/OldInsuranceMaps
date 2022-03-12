9b25bcfed245954c0624e010b18f49b6d57064d2
Adam Cox <mr.adamcox@gmail.com>
2022-03-12 11:52:32 -0600

    update admin list_display fields for each session type

--- georeference/admin.py
+++ georeference/admin.py
@@ -31,17 +31,19 @@ admin.site.register(MaskSession)
 
 class SessionAdmin(admin.ModelAdmin):
     readonly_fields = ('date_created', 'date_modified', 'date_run')
-    list_display = ('__str__', 'document', 'user', 'stage', 'status', 'note', 'date_created', 'date_modified', 'date_run')
+    list_display = ['__str__', 'document', 'layer', 'user', 'stage', 'status', 'note', 'date_created', 'date_modified', 'date_run']
     list_filter = ('stage', )
 
 class PrepSessionAdmin(SessionAdmin):
     exclude = ('type', 'layer')
+    list_display = [i for i in SessionAdmin.list_display if i != "layer"]
 
 class GeorefSessionAdmin(SessionAdmin):
     exclude = ('type', )
 
 class TrimSessionAdmin(SessionAdmin):
     exclude = ('type', 'document')
+    list_display = [i for i in SessionAdmin.list_display if i != "document"]
 
 admin.site.register(PrepSession, PrepSessionAdmin)
 admin.site.register(GeorefSession, GeorefSessionAdmin)
