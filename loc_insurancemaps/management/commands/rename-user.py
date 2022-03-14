b81b3d65fd818ae85070c7ec04bf7f4817abb950
Adam Cox <mr.adamcox@gmail.com>
2022-03-14 16:29:07 -0500

    improve user rename command, use new session model

--- loc_insurancemaps/management/commands/rename-user.py
+++ loc_insurancemaps/management/commands/rename-user.py
@@ -1,14 +1,12 @@
-import os
-import csv
-from re import S
+import logging
 
-from django.conf import settings
-from django.core import management
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 from django.db import transaction
 
-from georeference.models import GeoreferenceSession
+from georeference.models import GeorefSession
+
+logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'command to search the Library of Congress API.'
@@ -25,34 +23,40 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        old_name = options['old-username']
+        new_name = options['new-username']
+
         # lazy username.lower() query operation.
         user = None
         for u in get_user_model().objects.all():
-            if u.username.lower() == options['old-username'].lower():
+            if u.username.lower() == old_name.lower():
                 user = u
+                old_name = user.username
+                user.username = new_name
+                user.save()
+                logger.info(f"username updated.")
+        logger.info(f"renaming user: {old_name} --> {new_name}")
         if user is None:
-            print("no existing user matches this username")
-            exit()
+            logger.info("no existing user matches this username")
 
-        old_name = user.username
-        new_name = options['new-username']
-
-        for s in GeoreferenceSession.objects.all():
-            gcp_users = set([u['properties']['username'] for u in s.gcps_used['features']])
+        seshct = 0
+        ptct = 0
+        for s in GeorefSession.objects.all():
+            if not s.data:
+                continue
+            if not 'features' in s.data['gcps']:
+                continue
+            gcp_users = set([u['properties']['username'] for u in s.data['gcps']['features']])
             if old_name in gcp_users:
+                seshct += 1
                 try:
                     with transaction.atomic():
-                        print(f"updating gcps_used in GeoreferenceSession {s.pk}...")
-                        ct = 0
-                        for gcp in s.gcps_used['features']:
+                        for gcp in s.data['gcps']['features']:
                             if gcp['properties']['username'] == old_name:
                                 gcp['properties']['username'] = new_name
-                                ct += 1
-                        print(f"  {ct} updated.")
-                        s.save(update_fields=["gcps_used"])
-                        print(f"changing username {old_name} --> {new_name}...")
-                        user.username = new_name
-                        user.save()
-                        print(f"  update successful.")
+                                ptct += 1
+                        s.save(update_fields=["data"])
                 except Exception as e:
+                    logger.error(e)
                     raise(e)
+        logger.info(f"updated {ptct} GCPs across {seshct} sessions.")
