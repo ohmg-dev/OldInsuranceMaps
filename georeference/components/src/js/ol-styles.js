1b0bbe36643dc48c31fe0465554ca328a29e207f
Adam Cox <mr.adamcox@gmail.com>
2022-03-18 13:37:42 -0500

    improve GCP styles

--- georeference/components/src/js/ol-styles.js
+++ georeference/components/src/js/ol-styles.js
@@ -12,48 +12,64 @@ const interactionPointer = new Circle({
   radius: 5,
 })
 
+// this is the white outline cross that sits behind every gcp style
+const gcpOutline = new Style({
+  image: new RegularShape({
+    radius1: 10,
+    radius2: 0,
+    points: 4,
+    rotation: .79,
+    fill: new Fill({color: "#ffffff"}),
+    stroke: new Stroke({
+      color: "#ffffff", width: 5
+    })
+  })
+});
+
+// create the gcp styles, essentially different fill colors to sit
+// on top of the outline.
+const gcpDefault = new Style({
+  image: new RegularShape({
+    radius1: 10,
+    radius2: 0,
+    points: 4,
+    rotation: .79,
+    stroke: new Stroke({
+      color: 'black', width: 2
+    })
+  })
+})
+const gcpHover = new Style({
+  image: new RegularShape({
+    radius1: 10,
+    radius2: 0,
+    points: 4,
+    rotation: .79,
+    stroke: new Stroke({
+      color: 'red', width: 2
+    })
+  })
+})
+const gcpHighlight = new Style({
+  image: new RegularShape({
+    radius1: 10,
+    radius2: 0,
+    points: 4,
+    rotation: .79,
+    // fill: new Fill({color: '#00ff00'}),
+    stroke: new Stroke({
+      color: '#419EB6', width: 2
+    })
+  })
+})
+
 class Styles {
 
   empty = new Style();
 
-  gcpHover = new Style({
-    image: new RegularShape({
-      radius1: 10,
-      radius2: 1,
-      points: 4,
-      rotation: .79,
-      fill: new Fill({color: "#27ebe7"}),
-      stroke: new Stroke({
-        color: "#27ebe7", width: 2
-      })
-    })
-  });
-
-  gcpDefault = new Style({
-    image: new RegularShape({
-      radius1: 10,
-      radius2: 1,
-      points: 4,
-      rotation: .79,
-      fill: new Fill({color: 'black'}),
-      stroke: new Stroke({
-        color: 'black', width: 2
-      })
-    })
-  });
-
-  gcpHighlight = new Style({
-    image: new RegularShape({
-      radius1: 10,
-      radius2: 1,
-      points: 4,
-      rotation: .79,
-      fill: new Fill({color: "#bb2c80" }),
-      stroke: new Stroke({
-        color: "#bb2c80", width: 2
-      })
-    })
-  });
+  gcpDefault = [gcpOutline, gcpDefault];
+  gcpHover = [gcpOutline, gcpHover];
+  gcpHighlight = [gcpOutline, gcpHighlight];
 
   splitPreviewStyle = new Style({
     fill: new Fill({ color: 'rgba(255, 29, 51, 0.1)', }),
