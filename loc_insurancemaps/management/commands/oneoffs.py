538dcef6ded90b19fcc23f7ab97134803116ceb7
Adam Cox <mr.adamcox@gmail.com>
2022-05-24 14:24:34 -0500

    add command to use new thumbnail creation util
    Signed-off-by: Adam Cox <mr.adamcox@gmail.com>

--- loc_insurancemaps/management/commands/oneoffs.py
+++ loc_insurancemaps/management/commands/oneoffs.py
@@ -6,7 +6,10 @@ from django.template.loader import render_to_string
 from django.test.client import RequestFactory
 
 from geonode.documents.models import Document
+from geonode.layers.models import Layer
+
 from loc_insurancemaps.models import FullThumbnail, Volume
+from loc_insurancemaps.renderers import generate_layer_geotiff_thumbnail
 
 class Command(BaseCommand):
     help = 'command to search the Library of Congress API.'
@@ -15,16 +18,21 @@ class Command(BaseCommand):
         parser.add_argument(
             "operation",
             choices=[
-                "fix-fullthumbnails",
-                "fix-defaultthumbnails",
+                "fix-full-thumbnails",
+                "fix-document-thumbnails",
+                "fix-layer-thumbnails",
                 "generate-500.html",
             ],
             help="the identifier of the LoC resource to add",
         )
+        parser.add_argument(
+            "--layerid",
+            help="the identifier of for the layer",
+        )
 
     def handle(self, *args, **options):
 
-        if options['operation'] == "fix-fullthumbnails":
+        if options['operation'] == "fix-full-thumbnails":
             docs = Document.objects.all()
             print(f"{docs.count()} documents")
             ext = 0
@@ -50,15 +58,31 @@ class Command(BaseCommand):
                 v.populate_lookups()
             print("complete.")
 
-        if options['operation'] == "fix-defaultthumbnails":
+        if options['operation'] == "fix-document-thumbnails":
             docs = Document.objects.all()
             print(f"{docs.count()} documents")
             mis = 0
             for d in docs:
                 if "missing" in d.thumbnail_url:
-                   mis += 1
-                   d.save()
+                    mis += 1
+                    d.save()
             print(f"{mis} missing thumbnail(s) created")
+
+        if options['operation'] == "fix-layer-thumbnails":
+            if options['layerid'] is not None:
+                print(f"forcing regneration of {options['layerid']} thumbnail")
+                layers = Layer.objects.filter(alternate=options['layerid'])
+            else:
+                layers = Layer.objects.all()
+                print(f"checking {layers.count()} layers")
+                layers = [i for i in layers if "missing" in i.thumbnail_url]
+                print(f"{len(layers)} layers with missing thumbnails.")
+            fixed = 0
+            for l in layers:
+                print(f"regenerating thumb for {l.alternate}")
+                generate_layer_geotiff_thumbnail(l)
+                fixed += 1
+            print(f"{fixed} missing thumbnail(s) created")
 
         if options['operation'] == "generate-500.html":
             rf = RequestFactory()
