11c0264f13767c8fabe4f2707b607914921cfc18
Adam Cox <mr.adamcox@gmail.com>
2022-09-26 15:57:56 -0500

    initial creation of operation to consolidate all volume layers to single COG

--- loc_insurancemaps/management/commands/volume.py
+++ loc_insurancemaps/management/commands/volume.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 
-from loc_insurancemaps.management.volume import import_volume
+from loc_insurancemaps.tasks import generate_mosaic_geotiff_as_task
+from loc_insurancemaps.management.volume import import_volume, generate_mosaic_geotiff
 from loc_insurancemaps.models import Volume
 
 class Command(BaseCommand):
@@ -16,6 +17,7 @@ class Command(BaseCommand):
                 "import",
                 "refresh-lookups",
                 "make-sheets",
+                "generate-mosaic",
             ],
             help="the operation to perform",
         ),
@@ -28,6 +30,11 @@ class Command(BaseCommand):
             action="store_true",
             help="boolean to indicate whether documents should be made for the sheets",
         ),
+        parser.add_argument(
+            "--background",
+            action="store_true",
+            help="run the operation in the background with celery"
+        )
 
     def handle(self, *args, **options):
 
@@ -51,3 +58,12 @@ class Command(BaseCommand):
                 vol.load_date = datetime.now()
                 vol.save(update_fields=["loaded_by", "load_date"])
                 vol.load_sheet_documents()
+        if options['operation'] == "generate-mosaic":
+            if i is not None:
+                if options['background']:
+                    generate_mosaic_geotiff_as_task.apply_async(
+                        (i, ),
+                        queue="update"
+                    )
+                else:
+                    generate_mosaic_geotiff(i)
--- loc_insurancemaps/management/volume.py
+++ loc_insurancemaps/management/volume.py
@@ -1,5 +1,14 @@
+import os
+import json
 import logging
+from osgeo import gdal, osr, ogr
 
+from django.conf import settings
+from django.core.files import File
+
+from geonode.layers.models import Layer
+
+from georeference.proxy_models import LayerProxy
 from loc_insurancemaps.api import CollectionConnection
 from loc_insurancemaps.models import Volume
 from loc_insurancemaps.utils import LOCParser, filter_volumes_for_use, unsanitize_name
@@ -53,3 +62,94 @@ def import_volume(identifier):
     volume.regions.set(parsed.regions)
 
     return volume
+
+
+def multimask_to_geojson(identifier):
+    """Really, the multimask property on Volume should be reimplemented
+    so it is always storing a GeoJSON FeatureCollection with the layer
+    name in the properties of each feature. This helper function will
+    convert the existing multimask format for now."""
+
+    vol = Volume.objects.get(pk=identifier)
+    multimask_geojson = {"type": "FeatureCollection", "features": []}
+    for layer, geojson in vol.multimask.items():
+        geojson["properties"] = {"layer": layer}
+        multimask_geojson['features'].append(geojson)
+
+    return multimask_geojson
+
+def generate_mosaic_geotiff(identifier):
+    """ A helpful reference from the BPLv used during the creation of this method:
+    https://github.com/bplmaps/atlascope-utilities/blob/master/new-workflow/atlas-tools.py
+    """
+
+    vol = Volume.objects.get(pk=identifier)
+    multimask_geojson = multimask_to_geojson(identifier)
+    multimask_file_name = f"multimask-{identifier}"
+    multimask_file = os.path.join(settings.TEMP_DIR, f"{multimask_file_name}.geojson")
+    with open(multimask_file, "w") as out:
+        json.dump(multimask_geojson, out, indent=1)
+
+    trim_list = []
+    for feature in multimask_geojson['features']:
+
+        layer_name = feature['properties']['layer']
+        wo = gdal.WarpOptions(
+            format="VRT",
+            dstSRS = "EPSG:3857",
+            cutlineDSName = multimask_file,
+            cutlineLayer = multimask_file_name,
+            cutlineWhere = f"layer='{layer_name}'",
+            cropToCutline = True,
+            # creationOptions = ['COMPRESS=LZW', 'BIGTIFF=YES'],
+            # resampleAlg = 'cubic',
+            # dstAlpha = False,
+            dstNodata = "255 255 255",
+        )
+
+        lp = LayerProxy(f"geonode:{layer_name}")
+        layer_file = lp.get_layer_file()
+        if layer_file is None:
+            raise Exception(f"no layer file for this layer {layer_name}")
+        in_path = layer_file.path
+        trim_name = os.path.basename(in_path).replace(".tif", "_trim.vrt")
+        out_path = os.path.join(settings.TEMP_DIR, trim_name)
+        gdal.Warp(out_path, in_path, options=wo)
+
+        to = gdal.TranslateOptions(
+            format="VRT",
+            bandList = [1,2,3]
+        )
+
+        noalpha_path = out_path.replace(".vrt", "_noalpha.vrt")
+        gdal.Translate(noalpha_path, out_path, options=to)
+
+        trim_list.append(noalpha_path)
+
+    mosaic_vrt = os.path.join(settings.TEMP_DIR, f"{identifier}.vrt")
+    vo = gdal.BuildVRTOptions(
+        resolution = 'highest',
+        outputSRS = 'EPSG:3857',
+        separate = False,
+        srcNodata = "255 255 255",
+    )
+    print("building vrt")
+    gdal.BuildVRT(mosaic_vrt, trim_list, options=vo)
+
+    print("building final geotiff")
+    to = gdal.TranslateOptions(
+        format="GTiff",
+        creationOptions = [
+            "TILED=YES",
+            "COMPRESS=DEFLATE",
+            ## the following is apparently in the COG spec but doesn't work??
+            # "COPY_SOURCE_OVERVIEWS=YES",
+        ],
+    )
+
+    mosaic_tif = mosaic_vrt.replace(".vrt", ".tif")
+    gdal.Translate(mosaic_tif, mosaic_vrt, options=to)
+
+    with open(mosaic_tif, 'rb') as f:
+        vol.mosaic_geotiff = File(f, name=os.path.basename(mosaic_tif))
+        vol.save()
