8e4c6542aa7ec1f19b8f328b8a3f5aa2280687d9
Adam Cox <mr.adamcox@gmail.com>
2022-10-12 18:31:27 -0500

    remove unneeded db calls

--- loc_insurancemaps/context_processors.py
+++ loc_insurancemaps/context_processors.py
@@ -4,19 +4,9 @@ from .models import Volume, get_volume
 
 def loc_info(request):
 
-    volumes = Volume.objects.all()
-    id_list = list(volumes.order_by("city", "year").values_list("identifier", "city", "year", "volume_no"))
-
-    info = {
-        "volumes": {
-            "total_ct": volumes.count(),
-            "started_ct": volumes.exclude(loaded_by=None).count(),
-            "id_list": id_list,
-        }
-    }
-
     resource_type, res_id = analyze_url(request)
 
+    info = {}
     if res_id is not None:
         vol = get_volume(resource_type, res_id)
         if vol is not None:
