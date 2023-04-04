f64d00307a1d26d92549cd3d1f4308bb4e18875a
Adam Cox <mr.adamcox@gmail.com>
2023-04-04 10:36:55 -0500

    use api to get user list

--- frontend/components/src/Participants.svelte
+++ frontend/components/src/Participants.svelte
@@ -2,23 +2,30 @@
 import {TableSort} from 'svelte-tablesort';
 import TitleBar from './TitleBar.svelte';
 
-export let PARTICIPANTS;
+export let USER_API_URL;
 
-let participants = PARTICIPANTS;
+let all_participants = [];
+let filtered_participants = []
 
-console.log(participants)
+fetch(USER_API_URL)
+	.then(response => response.json())
+    .then(result => {
+		all_participants = result;
+		filtered_participants = result;
+	});
+
 function updateFilteredList(filterText) {
 	if (filterText && filterText.length > 0) {
-		participants = [];
-		PARTICIPANTS.forEach( function(p) {
+		filtered_participants = [];
+		all_participants.forEach( function(p) {
 			const name = p.username.toUpperCase();
 			const filterBy = filterText.toUpperCase();
 			if (name.indexOf(filterBy) > -1) {
-				participants.push(p);
+				filtered_participants.push(p);
 			}
 		});
 	} else {
-		participants = PARTICIPANTS;
+		filtered_participants = all_participants;
 	}
 }
 let filterInput;
@@ -29,15 +36,15 @@ function toggleList(userId) {
 	listEl.style.display = listEl.style.display == "flex" ? "none" : "flex";
 	const listBtnEl = document.getElementById(userId+"-btn");
 	listBtnEl.innerText = listBtnEl.innerText == "show all" ? "hide all" : "show all";
-} 
+}
 
 </script>
 <TitleBar TITLE={"Participants"} SIDE_LINKS={[]} ICON_LINKS={[]}/>
 <input type="text" id="filterInput" placeholder="Filter by username..." bind:value={filterInput}>
 <div style="overflow-x:auto;">
-	<TableSort items={participants}>
+	<TableSort items={filtered_participants}>
 		<tr slot="thead">
-			<th data-sort="username" style="max-width:300px;" title="Name of mapped location"></th>
+			<th data-sort="username" style="max-width:300px;"></th>
 			<th data-sort="username" style="max-width:300px;" title="Name of mapped location">Username</th>
 			<th data-sort="load_ct" title="Number of unprepared sheets">Loaded Volumes</th>
 			<th data-sort="psesh_ct" style="width:25px; text-align:center; border-left: 1px solid gray;" title="Number of prep sessions">Prep Sessions</th>
@@ -47,7 +54,7 @@ function toggleList(userId) {
 		</tr>
 		<tr slot="tbody" let:item={p} style="height:38px;">
 			<td>
-				<img style="height:30px; width:30px;" src={p.avatar} alt={p.username} />
+				<img style="height:30px; width:30px; border-radius:5px;" src={p.image_url} alt={p.username} />
 			</td>
 			<td>
 				<a href={p.profile_url} alt="Profile for {p.username}" title="Profile for {p.username}">
@@ -82,6 +89,9 @@ function toggleList(userId) {
 			<td style="text-align:center; border-left:1px solid gray;">{p.gcp_ct}</td>
 		</tr>
 	</TableSort>
+	{#if all_participants.length === 0}
+	<div class='lds-ellipsis'><div></div><div></div><div></div><div></div></div>
+	{/if}
 </div>
 <style>
 	.full-volume-list {
