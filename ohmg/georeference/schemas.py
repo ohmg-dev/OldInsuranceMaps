459ac4948a958f3cf9debfc1cdccac518657538e
Adam Cox <mr.adamcox@gmail.com>
2023-09-12 07:40:14 -0500

    add relative timestamp display

--- ohmg/georeference/schemas.py
+++ ohmg/georeference/schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import List, Optional
 
 from django.urls import reverse
@@ -56,18 +57,42 @@ class SessionSchema(Schema):
     type: str
     user: UserSchema
     note: str = None
+    # resource_id = int
     doc: DocumentSchema = None
     lyr: LayerSchema = None
     status: str
+    stage: str
     data: dict
     user_input_duration: int = None
-    date_created: str = None
+    date_created: dict = None
 
     @staticmethod
     def resolve_date_created(obj):
-        return obj.date_created.strftime("%Y-%m-%d")
+        d = {
+            'date': obj.date_created.strftime("%Y-%m-%d"),
+            'relative': ''
+        }
+        diff = datetime.now() - obj.date_created
+
+        if diff.days > 0:
+            n, u = diff.days, 'day'
+        else:
+            seconds = diff.total_seconds()
+            hours = seconds // 3600
+            minutes = (seconds % 3600) // 60
+            seconds = seconds % 60
+            if hours > 0:
+                n, u = hours, "hour"
+            elif minutes > 0:
+                n, u = minutes, "minute"
+            else:
+                n, u = seconds, "second"
+        n = int(n)
+        d['relative'] = f"{n} {u}{'' if n == 1 else 's'} ago"
+        return d
 
 class FilterSessionSchema(FilterSchema):
     username: Optional[str] = Field(q='user__username') 
     item: Optional[List[int]] = Field(q=['doc_id', 'lyr_id'])
+    resource: Optional[List[int]] = Field(q=['doc_id__in', 'lyr_id__in'])
     type: Optional[str]
