908696993bed0d500999f3f03a7fdfea5c18fe25
Adam Cox <mr.adamcox@gmail.com>
2023-12-14 00:25:53 -0600

    implement dotenv

--- manage.py
+++ manage.py
@@ -3,9 +3,11 @@
 import os
 import sys
 
+from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
+    load_dotenv()
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ohmg.settings')
     try:
         from django.core.management import execute_from_command_line
