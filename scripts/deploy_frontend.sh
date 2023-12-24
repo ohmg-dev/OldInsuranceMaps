fab5e6c62e1835adc6e5b9b51bfed820d2bb7c3e
Adam Cox <mr.adamcox@gmail.com>
2023-12-24 11:56:33 -0600

    add script to build and deploy static assets

---
+++ scripts/deploy_frontend.sh
@@ -0,0 +1,26 @@
+#! /usr/bin/bash
+
+# activating the virtual environment should set $VIRTUAL_ENV
+# to /path/to/env, use this to find the python executable
+PYTHON=$VIRTUAL_ENV/bin/python
+# full path to this script. anticipate app paths from here
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT=$SCRIPT_DIR/..
+
+echo "building frontend"
+CURRENT_DIR=$PWD
+cd $PROJECT_ROOT/ohmg/frontend/svelte
+pnpm install
+rm -rf ./public/build
+pnpm run build
+cd $CURRENT_DIR
+
+echo "running collectstatic"
+$VIRTUAL_ENV/bin/python $PROJECT_ROOT/manage.py collectstatic --noinput
+
+echo "update build number"
+$VIRTUAL_ENV/bin/python $PROJECT_ROOT/manage.py update_build
+
+echo "reset uwsgi service"
+touch $PROJECT_ROOT/ohmg/wsgi.py
+
