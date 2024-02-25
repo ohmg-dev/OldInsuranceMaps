f6671477544496e0c23d4f848363cb10ebbad29e
Adam Cox <mr.adamcox@gmail.com>
2024-02-25 14:22:42 +0000

    implement published page field

--- ohmg/content/models.py
+++ ohmg/content/models.py
@@ -54,8 +54,10 @@ class Page(models.Model):
 
     title = models.CharField(
         max_length=200,
-        help_text="Title will be slugified and must not conflict with other Pages or Places."\
-            "Place slugs are formatted as follows: 'united-states', 'orleans-parish-la', and 'new-orleans-la'."
+        help_text="Title will be slugified and must not conflict with "\
+            "other Pages or Places. Place slugs are formatted as "\
+            "follows: 'united-states', 'orleans-parish-la', and "\
+            "'new-orleans-la'."
     )
     slug = models.CharField(
         max_length=200,
@@ -63,10 +65,13 @@ class Page(models.Model):
         blank=True
     )
     content = MarkdownxField()
+    published = models.BooleanField(
+        default=False
+    )
 
     def __str__(self):
         return self.title
-    
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         return super(Page, self).save(*args, **kwargs)
--- ohmg/content/urls.py
+++ ohmg/content/urls.py
@@ -9,19 +9,19 @@ from .views import (
 )
 
 from .models import Page
- 
+
 class PageConverter(StringConverter):
- 
+
     def to_python(self, value):
         try:
             # returns the actual object and passes directly to view
-            return Page.objects.get(slug=value)
+            return Page.objects.get(slug=value, published=True)
         except Page.DoesNotExist:
             raise ValueError
- 
+
     def to_url(self, obj):
         return str(obj.slug)
-    
+
 register_converter(PageConverter, 'page-slug')
 
 urlpatterns = [
