e79172014349c9933686848a646ececd835460ed
Adam Cox <mr.adamcox@gmail.com>
2024-08-24 20:56:51 +0000

    allow skipping docs during migration; fix gdal.open param

--- ohmg/core/management/commands/migrate_core_content.py
+++ ohmg/core/management/commands/migrate_core_content.py
@@ -35,6 +35,11 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
+            "--skip-docs",
+            action="store_true",
+            help="skip the part of the process that migrations Document and Region instances",
+        )
+        parser.add_argument(
             "--clean",
             action="store_true",
             help="deletes all instances of new models before running the migration operations",
@@ -77,101 +82,102 @@ class Command(BaseCommand):
 
         print_to_log(f"starting migration: {datetime.now()}")
 
-        for vol in Volume.objects.all():
-            print(f"Processing: {vol}")
-            document_sources = vol.lc_resources[0].get("files") if vol.lc_resources else None
-            map, created = Map.objects.get_or_create(
-                title=vol.__str__(),
-                identifier=vol.identifier,
-                volume_number=vol.volume_no,
-                document_page_type="page",
-                year=vol.year,
-                month=vol.month,
-                loaded_by=vol.loaded_by,
-                load_date=vol.load_date,
-                document_sources=document_sources
-            )
-            if not created:
-                print("  -- using existing map object")
-            map.locales.set(vol.locales.all())
+        if not options['skip_docs']:
+            for vol in Volume.objects.all():
+                print(f"Processing: {vol}")
+                document_sources = vol.lc_resources[0].get("files") if vol.lc_resources else None
+                map, created = Map.objects.get_or_create(
+                    title=vol.__str__(),
+                    identifier=vol.identifier,
+                    volume_number=vol.volume_no,
+                    document_page_type="page",
+                    year=vol.year,
+                    month=vol.month,
+                    loaded_by=vol.loaded_by,
+                    load_date=vol.load_date,
+                    document_sources=document_sources
+                )
+                if not created:
+                    print("  -- using existing map object")
+                map.locales.set(vol.locales.all())
 
-            for sheet in vol.sheets:
-                if sheet.doc:
-                    # hacky method for pulling out the sheet number from the title
-                    page_number = ""
-                    try:
-                        page_number = sheet.doc.title.split("|")[-1].split("p")[1]
-                    except IndexError:
-                        print_to_log(f"[ERROR] can't get page number from doc title {sheet.doc} ({sheet.doc.pk})")
-                    if page_number == "":
-                        page_number = "0"
-                    new_doc = Document.objects.create(
-                        pk=sheet.doc.pk,
-                        map=map,
-                        page_number=page_number,
-                    )
-                    if not options['no_files']:
-                        save_file_to_object(new_doc, source_object=sheet.doc)
+                for sheet in vol.sheets:
+                    if sheet.doc:
+                        # hacky method for pulling out the sheet number from the title
+                        page_number = ""
+                        try:
+                            page_number = sheet.doc.title.split("|")[-1].split("p")[1]
+                        except IndexError:
+                            print_to_log(f"[ERROR] can't get page number from doc title {sheet.doc} ({sheet.doc.pk})")
+                        if page_number == "":
+                            page_number = "0"
+                        new_doc = Document.objects.create(
+                            pk=sheet.doc.pk,
+                            map=map,
+                            page_number=page_number,
+                        )
+                        if not options['no_files']:
+                            save_file_to_object(new_doc, source_object=sheet.doc)
 
-                    # find the existing session for this document and attach the new doc to it
-                    prep_sessions = PrepSession.objects.filter(doc=sheet.doc)
-                    if len(prep_sessions) > 1:
-                        print_to_log(f"[ERROR] Too many prep sessions for this document: {sheet.doc} ({sheet.doc.pk})")
-                    if prep_sessions.exists():
-                        prep_sessions[0].doc2 = new_doc
-                        prep_sessions[0].save()
+                        # find the existing session for this document and attach the new doc to it
+                        prep_sessions = PrepSession.objects.filter(doc=sheet.doc)
+                        if len(prep_sessions) > 1:
+                            print_to_log(f"[ERROR] Too many prep sessions for this document: {sheet.doc} ({sheet.doc.pk})")
+                        if prep_sessions.exists():
+                            prep_sessions[0].doc2 = new_doc
+                            prep_sessions[0].save()
 
-                    # if there is only one real document for this sheet, then use it to make
-                    # a new Region, but only if that one document is not "unprepared".
-                    if len(sheet.real_docs) == 1:
-                        if sheet.doc.status != "unprepared":
-                            # use a fake boundary if there aren't local files available
-                            if options['no_files']:
-                                w, h = 10, 10
-                            else:
-                                w, h = sheet.doc.image_size
-                            is_map = True
-                            if sheet.doc.vrs and sheet.doc.vrs.category.slug == "non-map-content":
-                                is_map = False
-                            region = Region.objects.create(
-                                boundary = Polygon([[0,0], [0,h], [w,h], [w,0], [0,0]]),
-                                document=new_doc,
-                                gcp_group=sheet.doc.gcp_group,
-                                is_map=is_map,
-                            )
-                            if not options['no_files']:
-                                save_file_to_object(region, source_object=new_doc)
+                        # if there is only one real document for this sheet, then use it to make
+                        # a new Region, but only if that one document is not "unprepared".
+                        if len(sheet.real_docs) == 1:
+                            if sheet.doc.status != "unprepared":
+                                # use a fake boundary if there aren't local files available
+                                if options['no_files']:
+                                    w, h = 10, 10
+                                else:
+                                    w, h = sheet.doc.image_size
+                                is_map = True
+                                if sheet.doc.vrs and sheet.doc.vrs.category.slug == "non-map-content":
+                                    is_map = False
+                                region = Region.objects.create(
+                                    boundary = Polygon([[0,0], [0,h], [w,h], [w,0], [0,0]]),
+                                    document=new_doc,
+                                    gcp_group=sheet.doc.gcp_group,
+                                    is_map=is_map,
+                                )
+                                if not options['no_files']:
+                                    save_file_to_object(region, source_object=new_doc)
 
-                    # if there are more than one real documents for this sheet, make a new
-                    # Region for both of them, because these documents represent split children
+                        # if there are more than one real documents for this sheet, make a new
+                        # Region for both of them, because these documents represent split children
+                        else:
+                            divisions = sheet.doc.preparation_session.data['divisions']
+                            if not len(divisions) == len(sheet.real_docs):
+                                print_to_log(f"[ERROR] Division mismatch: {len(divisions)} divisions, {len(sheet.real_docs)} real_docs (sheet: {sheet.pk})")
+                            for div_no, div in enumerate(divisions, start=1):
+                                matching_old_doc = [i for i in sheet.real_docs if int(i.slug.split("_")[-1]) == div_no][0]
+                                is_map = True
+                                if matching_old_doc.vrs and matching_old_doc.vrs.category.slug == "non-map-content":
+                                    is_map = False
+                                div_poly = Polygon(div)
+                                region = Region.objects.create(
+                                    boundary=div_poly,
+                                    document=new_doc,
+                                    division_number=div_no,
+                                    gcp_group=matching_old_doc.gcp_group,
+                                    created_by=matching_old_doc.preparation_session.user,
+                                    is_map=is_map
+                                )
+                                if not options['no_files']:
+                                    save_file_to_object(region, source_object=matching_old_doc)
                     else:
-                        divisions = sheet.doc.preparation_session.data['divisions']
-                        if not len(divisions) == len(sheet.real_docs):
-                            print_to_log(f"[ERROR] Division mismatch: {len(divisions)} divisions, {len(sheet.real_docs)} real_docs (sheet: {sheet.pk})")
-                        for div_no, div in enumerate(divisions, start=1):
-                            matching_old_doc = [i for i in sheet.real_docs if int(i.slug.split("_")[-1]) == div_no][0]
-                            is_map = True
-                            if matching_old_doc.vrs and matching_old_doc.vrs.category.slug == "non-map-content":
-                                is_map = False
-                            div_poly = Polygon(div)
-                            region = Region.objects.create(
-                                boundary=div_poly,
-                                document=new_doc,
-                                division_number=div_no,
-                                gcp_group=matching_old_doc.gcp_group,
-                                created_by=matching_old_doc.preparation_session.user,
-                                is_map=is_map
-                            )
-                            if not options['no_files']:
-                                save_file_to_object(region, source_object=matching_old_doc)
-                else:
-                    print_to_log(f"[WARNING] this sheet has no document {sheet} ({sheet.pk})")
+                        print_to_log(f"[WARNING] this sheet has no document {sheet} ({sheet.pk})")
 
         # now, separately, iterate all old layers and create new ones from them
         existing_layers = LayerV1.objects.all()
         matched = []
         for old_layer in existing_layers:
-
+            print(old_layer, old_layer.pk)
             old_doc = old_layer.get_document()
             if not old_doc:
                 print_to_log(f"[ERROR] This layer has no doc: {old_layer} ({old_layer.pk})")
--- ohmg/core/renderers.py
+++ ohmg/core/renderers.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 
 def get_extent_from_file(file_path: Path):
     """Credit: https://gis.stackexchange.com/a/201320/28414 """
-    src = gdal.Open(file_path)
+    src = gdal.Open(str(file_path))
     ulx, xres, xskew, uly, yskew, yres  = src.GetGeoTransform()
     lrx = ulx + (src.RasterXSize * xres)
     lry = uly + (src.RasterYSize * yres)
