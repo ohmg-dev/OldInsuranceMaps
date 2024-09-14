bbcb70119720deefd6d4be82ee6ad58ef7353d00
Adam Cox <mr.adamcox@gmail.com>
2024-09-14 15:39:43 -0500

    update migration helper script

--- ohmg/core/management/commands/resave_content.py
+++ ohmg/core/management/commands/resave_content.py
@@ -19,31 +19,44 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        existing_bboxs = LayerV1.objects.all().values_list('slug', 'x0', 'y0', 'x1', 'y1')
+        # existing_bboxs = LayerV1.objects.all().values_list('slug', 'x0', 'y0', 'x1', 'y1')
 
-        bbox_lookup = {i[0]: [float(i[1]), float(i[2]), float(i[3]), float(i[4])] for i in existing_bboxs}
-        
-        models = [
-            # Document,
-            # Region,
-            # Layer,
-            LayerSet,
-        ]
-        for model in models:
-            print(model)
-            instances = model.objects.all()
-            for i in instances:
-                # if i.slug in bbox_lookup:
-                #     i.extent = bbox_lookup[i.slug]
-                i.save()
-                # for layer in i.layers.all():
-                #     if layer.extent:
-                #         poly = Polygon().from_bbox(layer.extent)
-                #         print(poly)
+        # bbox_lookup = {i[0]: [float(i[1]), float(i[2]), float(i[3]), float(i[4])] for i in existing_bboxs}
 
-        # for map in Map.objects.all().prefetch_related():
-        #     print(map)
-        #     if map.documents:
-        #         if all([i.file is not None for i in map.documents.all()]):
-        #             map.set_status("ready")
-        #     map.update_item_lookup()
+        print("re-saving Documents...")
+        docs = Document.objects.all()
+        docs_ct = docs.count()
+        for ct, document in enumerate(docs, start=1):
+            print(f"{ct}/{docs_ct} (doc: {document.pk})")
+            document.save()
+
+        print("re-saving Regions...")
+        regs = Region.objects.all()
+        regs_ct = regs.count()
+        for ct, region in enumerate(regs, start=1):
+            print(f"{ct}/{regs_ct} (reg: {region.pk})")
+            region.save()
+
+        print("re-saving Layers...")
+        lyrs = Layer.objects.all()
+        lyrs_ct = lyrs.count()
+        for ct, layer in enumerate(lyrs, start=1):
+            print(f"{ct}/{lyrs_ct} (lyr: {layer.pk})")
+            layer.save()
+
+        print("re-saving LayerSets...")
+        layersets = Layer.objects.all()
+        layersets_ct = layersets.count()
+        for ct, layerset in enumerate(layersets, start=1):
+            print(f"{ct}/{layersets_ct} (ls: {layerset.pk})")
+            layerset.save()
+
+        print("updating all Map lookups...")
+        maps = Map.objects.all().prefetch_related()
+        maps_ct = maps.count()
+        for ct, map in enumerate(maps, start=1):
+            print(f"{ct}/{maps_ct} (map: {map.pk})")
+            if map.documents:
+                if all([i.file is not None for i in map.documents.all()]):
+                    map.set_status("ready")
+            map.update_item_lookup()
