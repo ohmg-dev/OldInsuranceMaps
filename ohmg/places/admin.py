9146f55a544d66c86589b887d14b1501966ca64d
Adam Cox <mr.adamcox@gmail.com>
2024-10-22 02:51:02 +0000

    more admin updates

--- ohmg/places/admin.py
+++ ohmg/places/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from ohmg.places.models import Place
 
 class PlaceAdmin(admin.ModelAdmin):
+    autocomplete_fields = ('direct_parents',)
     readonly_fields = ('display_name', 'slug')
     search_fields = ('display_name', 'slug')
     list_display = ('display_name', 'slug')
