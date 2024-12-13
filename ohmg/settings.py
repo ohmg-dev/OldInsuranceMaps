401e5cee4cb75f969bbacae8309533a7f900ba66
Adam Cox <mr.adamcox@gmail.com>
2024-12-13 17:05:19 -0600

    move context_processors from core app

--- ohmg/settings.py
+++ ohmg/settings.py
@@ -112,7 +112,7 @@ TEMPLATES = [
                 "django.template.context_processors.static",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "ohmg.core.context_processors.site_info",
+                "ohmg.context_processors.site_info",
             ],
             "debug": DEBUG,
         },
