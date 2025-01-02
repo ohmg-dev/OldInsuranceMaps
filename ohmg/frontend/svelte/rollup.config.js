d80047a3af8c467205678a3824131a4e3b03ff04
Adam Cox <mr.adamcox@gmail.com>
2025-01-02 11:06:16 -0600

    replace rollup-plugin-terser with @rollup/plugin-terser

--- ohmg/frontend/svelte/rollup.config.js
+++ ohmg/frontend/svelte/rollup.config.js
@@ -1,8 +1,8 @@
 import svelte from 'rollup-plugin-svelte';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
 import alias from '@rollup/plugin-alias';
 import path from "path";
