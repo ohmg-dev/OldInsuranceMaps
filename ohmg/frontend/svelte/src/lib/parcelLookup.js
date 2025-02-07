10c768cd0f033ef551e3b3d8af9970382a98c1c9
Adam Cox <mr.adamcox@gmail.com>
2025-02-07 19:55:48 +0000

    add more parcel reference layers

--- ohmg/frontend/svelte/src/lib/parcelLookup.js
+++ ohmg/frontend/svelte/src/lib/parcelLookup.js
@@ -8,5 +8,15 @@ export const parcelLookup = {
         "pmtilesUrl": "https://oldinsurancemaps.net/uploaded/phoenix-az.pmtiles",
         "attributionText": "City of Phoenix Open Data",
         "attributionUrl": "https://phoenixopendata.com/dataset/city-parcels/resource/a805b9a1-5a2f-44dc-8e43-6d8a2280c421?inner_span=True"
+    },
+    "chicago-il": {
+        "pmtilesUrl": "https://oldinsurancemaps.net/uploaded/chicago-il.pmtiles",
+        "attributionText": "Cook County GIS",
+        "attributionUrl": "https://hub-cookcountyil.opendata.arcgis.com/datasets/cookcountyil::historical-parcels-2021/about"
+    },
+    "athens-ga": {
+        "pmtilesUrl": "https://oldinsurancemaps.net/uploaded/athens-ga.pmtiles",
+        "attributionText": "Athens-Clarke County Open Data",
+        "attributionUrl": "https://data-athensclarke.opendata.arcgis.com/datasets/AthensClarke::acc-parcels/about"
     }
 }
