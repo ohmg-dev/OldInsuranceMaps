eed9b6fa51b450d59cf63995fe06145c4aeb5bf4
Adam Cox <mr.adamcox@gmail.com>
2025-04-09 07:25:04 -0500

    update parcel layer urls

--- ohmg/frontend/svelte/src/lib/parcelLookup.js
+++ ohmg/frontend/svelte/src/lib/parcelLookup.js
@@ -1,49 +1,60 @@
 export const parcelLookup = {
   'san-francisco-ca': {
-    pmtilesUrl: 'https://oldinsurancemaps.net/uploaded/san-francisco-ca.pmtiles',
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/sf_parcels.pmtiles',
     attributionText: 'City and County of San Francisco',
     attributionUrl:
       'https://data.sfgov.org/Geographic-Locations-and-Boundaries/Parcels-Active-and-Retired/acdm-wktn/about_data',
   },
+  'new-orleans-la': {
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/new-orleans-parcels.pmtiles',
+    attributionText: 'City of New Orleans',
+    attributionUrl:
+      'https://portal-nolagis.opendata.arcgis.com/datasets/273cc01f8dae4765b2924ba5de028c27/explore?layer=0',
+  },
   'phoenix-az': {
-    pmtilesUrl: 'https://oldinsurancemaps.net/uploaded/phoenix-az.pmtiles',
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/phoenix-az.pmtiles',
     attributionText: 'City of Phoenix Open Data',
     attributionUrl:
       'https://phoenixopendata.com/dataset/city-parcels/resource/a805b9a1-5a2f-44dc-8e43-6d8a2280c421?inner_span=True',
   },
   'chicago-il': {
-    pmtilesUrl: 'https://oldinsurancemaps.net/uploaded/chicago-il.pmtiles',
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/chicago-il.pmtiles',
     attributionText: 'Cook County GIS',
     attributionUrl: 'https://hub-cookcountyil.opendata.arcgis.com/datasets/cookcountyil::historical-parcels-2021/about',
   },
   'athens-ga': {
-    pmtilesUrl: 'https://oldinsurancemaps.net/uploaded/athens-ga.pmtiles',
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/clark-county-ga.pmtiles',
     attributionText: 'Athens-Clarke County Open Data',
     attributionUrl: 'https://data-athensclarke.opendata.arcgis.com/datasets/AthensClarke::acc-parcels/about',
   },
   'detroit-mi': {
-    pmtilesUrl: 'https://oldinsurancemaps.net/uploaded/detroit-mi-parcels.pmtiles',
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/detroit-mi-parcels.pmtiles',
     attributionText: 'City of Detroit Open Data Portal',
     attributionUrl: 'https://data-detroitmi.hub.arcgis.com/datasets/parcels-current-1/explore',
   },
   'philadelphia-pa': {
-    pmtilesUrl: 'https://oldinsurancemaps.net/uploaded/philadelphia-pa-parcels.pmtiles',
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/philadelphia-pa-parcels.pmtiles',
     attributionText: 'Open Data Philly',
     attributionUrl: 'https://opendataphilly.org/datasets/department-of-records-property-parcels/',
   },
   'camden-nj': {
-    pmtilesUrl: 'https://oldinsurancemaps.net/uploaded/camden-nj-parcels.pmtiles',
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/camden-nj-parcels.pmtiles',
     attributionText: 'Camden County Open Data Portal',
     attributionUrl: 'https://camdencountynj-ccdpw.opendata.arcgis.com/',
   },
   'gloucester-county-nj': {
-    pmtilesUrl: 'https://oldinsurancemaps.net/uploaded/gloucester-county-parcels.pmtiles',
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/gloucester-county-parcels.pmtiles',
     attributionText: 'New Jersey Office of GIS',
     attributionUrl: 'https://njogis-newjersey.opendata.arcgis.com/documents/f74e347c0622425cbcb6d59d93ca6ac0/about',
   },
   'salem-county-nj': {
-    pmtilesUrl: 'https://oldinsurancemaps.net/uploaded/salem-county-parcels.pmtiles',
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/salem-county-parcels.pmtiles',
     attributionText: 'New Jersey Office of GIS',
     attributionUrl: 'https://www.arcgis.com/home/item.html?id=af31e6cfba5044a3914ba1ff52f5e26e',
+  },
+  ohio: {
+    pmtilesUrl: 'https://oldinsurancemaps.us-southeast-1.linodeobjects.com/parcels/ohio-parcels.pmtiles',
+    attributionText: 'Ohio Geographically Referenced Information Program',
+    attributionUrl: 'https://geohio.maps.arcgis.com/home/item.html?id=26ab5fad8d5d4258a7492a14de83bc0e',
   },
 };
