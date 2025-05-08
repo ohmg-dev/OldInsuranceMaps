498bdce0bb8c8b844c9685f4b2cba3bf4e972d74
Adam Cox <mr.adamcox@gmail.com>
2025-05-07 22:16:44 -0500

    remove obsolete configuration options and methods

--- ohmg/core/management/commands/configure.py
+++ ohmg/core/management/commands/configure.py
@@ -13,11 +13,7 @@ from django.contrib.sessions.middleware import SessionMiddleware
 
 
 class Command(BaseCommand):
-    help = (
-        "generate various system configuration files that incorporate the "
-        "the current app settings."
-    )
-    out_dir = "_system-configs"
+    help = "Perform various operations needed for configuration of the app."
     verbose = False
     python_env = Path(sys.executable).parent
 
@@ -25,34 +21,19 @@ class Command(BaseCommand):
         parser.add_argument(
             "type",
             choices=[
-                "supervisor",
-                "celery",
-                "uwsgi",
-                #                "nginx",
-                #                "nginx-ssl",
-                "all",
+                "services",
                 "generate-error-pages",
                 "initialize-s3-bucket",
                 "get-plugins",
-                "services",
             ],
             nargs="+",
-            help="Choose what configurations to generate.",
+            help="Choose what configuration operation to run.",
         )
         parser.add_argument(
             "-d",
-            "--directory",
-            help="Directory where the generated files will be placed.",
-        )
-        parser.add_argument(
-            "--cert-file",
-            help="Full path to the SSL certificate to be used in the nginx config",
-        )
-        parser.add_argument(
-            "--deploy",
-            action="store_true",
-            default=False,
-            help="Attempts to deploy the new settings to system locations.",
+            "--destination",
+            default=".services",
+            help="Directory where generated service files will be placed.",
         )
         parser.add_argument(
             "--verbose",
@@ -61,9 +42,7 @@ class Command(BaseCommand):
             help="Verbose output.",
         )
 
-    def _write_file(self, content: str, filename: str):
-        Path(self.out_dir).mkdir(exist_ok=True)
-        outpath = Path(self.out_dir, filename)
+    def _write_file(self, content: str, outpath: str):
         with open(outpath, "w") as o:
             o.write(content)
         return outpath
@@ -72,67 +51,10 @@ class Command(BaseCommand):
         value = getattr(settings, name, "<not in django settings>")
         if value == "<not in django settings>":
             value = os.getenv(name, default_value)
-
         return value
 
     def handle(self, *args, **options):
         self.verbose = options["verbose"]
-        create_all = "all" in options["type"]
-
-        if options["directory"] is not None:
-            self.out_dir = os.path.abspath(options["directory"])
-
-        if not os.path.isdir(self.out_dir):
-            os.mkdir(self.out_dir)
-
-        outputs = []
-        if create_all or "supervisor" in options["type"]:
-            if self.verbose:
-                print("creating supervisord service")
-            s_file = self.write_supervisor_config()
-            outputs.append(s_file)
-
-            ds_file = self.write_supervisor_deploy(s_file)
-            outputs.append(ds_file)
-
-        if create_all or "celery" in options["type"]:
-            if self.verbose:
-                print("creating celery config")
-            c_file = self.write_project_celery_config()
-            outputs.append(c_file)
-
-            dc_file = self.write_celery_deploy(c_file)
-            outputs.append(dc_file)
-
-        if create_all or "uwsgi" in options["type"]:
-            if self.verbose:
-                print("creating uwsgi ini and service configs")
-            ini_file = self.write_project_uwsgi_ini()
-            outputs.append(ini_file)
-
-            us_file = self.write_uwsgi_service(ini_file)
-            outputs.append(us_file)
-
-            du_file = self.write_uwsgi_service_deploy(us_file)
-            outputs.append(du_file)
-
-        if create_all or "nginx" in options["type"]:
-            if self.verbose:
-                print("creating nginx site config")
-            nginx_site_file = self.write_nginx_site_conf()
-            outputs.append(nginx_site_file)
-
-        if create_all or "nginx-ssl" in options["type"]:
-            if self.verbose:
-                print("creating nginx SSL site config")
-            nginx_ssl_site_file = self.write_nginx_ssl_site_conf()
-            outputs.append(nginx_ssl_site_file)
-
-        if self.verbose:
-            print(f"~~~\noutput directory: {os.path.abspath(self.out_dir)}")
-            print(f"~~~\nfile count: {len(outputs)}")
-            for f in outputs:
-                print(os.path.basename(f))
 
         if options["type"] == "generate-error-pages":
             self.generate_error_pages()
@@ -144,137 +66,19 @@ class Command(BaseCommand):
             self.get_plugins()
 
         if "services" in options["type"]:
-            self.create_services()
+            out_dir = Path(options["destination"])
+            output_files = self.create_services(destination=out_dir)
+
+            if self.verbose:
+                print(f"~~~\noutput directory: {out_dir.absolute()}")
+                print(f"~~~\nfile count: {len(output_files)}")
+                for f in output_files:
+                    print(f.name)
 
     def write_file(self, file_path, content):
         with open(file_path, "w") as out:
             out.write(content)
-
         return os.path.abspath(file_path)
-
-    def write_supervisor_config(self):
-        """
-        Writes a supervisor conf file with all necessary environment variables.
-        """
-
-        vars = [
-            ("DEBUG", False),
-            ("SITEURL", ""),
-            ("DJANGO_SETTINGS_MODULE", None),
-            ("BROKER_URL", ""),
-            ("DATABASE_NAME", ""),
-            ("DATABASE_USER", ""),
-            ("DATABASE_PASSWORD", ""),
-            ("DATABASE_HOST", ""),
-            ("DATABASE_PORT", ""),
-            ("CACHE_DIR", ""),
-            ("MEDIA_ROOT", ""),
-        ]
-
-        if self._resolve_var("EMAIL_ENABLE", False) is True:
-            vars += [
-                ("EMAIL_ENABLE", True),
-                ("DJANGO_EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend"),
-                ("DJANGO_EMAIL_HOST", "localhost"),
-                ("DJANGO_EMAIL_PORT", 587),
-                ("DJANGO_EMAIL_HOST_USER", ""),
-                ("DJANGO_EMAIL_HOST_PASSWORD", ""),
-                ("DJANGO_EMAIL_USE_TLS", False),
-                ("DJANGO_EMAIL_USE_SSL", False),
-                ("DEFAULT_FROM_EMAIL", "admin@localhost"),
-            ]
-
-        kv_list = []
-        for var in vars:
-            value = self._resolve_var(var[0], var[1])
-            kv_list.append(f'{var[0]}="{value}"')
-        env_block = "\n    " + ",\n    ".join(kv_list)
-
-        file_content = f"""; supervisor config file
-
-[unix_http_server]
-file=/var/run/supervisor.sock   ; (the path to the socket file)
-chmod=0700                       ; sockef file mode (default 0700)
-
-[supervisord]
-nodaemon=true
-logfile=/var/log/supervisor/supervisord.log ; (main log file;default $CWD/supervisord.log)
-pidfile=/var/run/supervisord.pid ; (supervisord pidfile;default supervisord.pid)
-childlogdir=/var/log/supervisor            ; ('AUTO' child log dir, default $TEMP)
-environment={env_block}
-
-; the below section must remain in the config file for RPC
-; (supervisorctl/web interface) to work, additional interfaces may be
-; added by defining them in separate rpcinterface: sections
-[rpcinterface:supervisor]
-supervisor.rpcinterface_factory = supervisor.rpcinterface:make_main_rpcinterface
-
-[supervisorctl]
-serverurl=unix:///var/run/supervisor.sock ; use a unix:// URL  for a unix socket
-
-; The [include] section can just contain the "files" setting.  This
-; setting can list multiple files (separated by whitespace or
-; newlines).  It can also contain wildcards.  The filenames are
-; interpreted as relative to this file.  Included files *cannot*
-; include files themselves.
-
-[include]
-files = /etc/supervisor/conf.d/*.conf
-"""
-
-        outfile_path = os.path.join(self.out_dir, "supervisord.conf")
-        full_path = self.write_file(outfile_path, file_content)
-
-        return full_path
-
-    def write_supervisor_deploy(self, conf_path):
-        deploy_path = os.path.join(self.out_dir, "deploy-supervisor.sh")
-        deploy_content = f"""#!/bin/bash
-sudo cp {conf_path} /etc/supervisor/supervisord.conf
-
-# kill celery workers and reload supervisor
-sudo pkill celery -f
-sudo supervisorctl reload
-"""
-        full_deploy_path = self.write_file(deploy_path, deploy_content)
-
-        return full_deploy_path
-
-    def write_project_celery_config(self):
-        project_name = settings.WSGI_APPLICATION.split(".")[0]
-        env_file = settings.BASE_DIR / ".env"
-        user = self._resolve_var("USER", "username")
-        celery_path = os.path.join(os.path.dirname(sys.executable), "celery")
-
-        file_content = f"""[program:{project_name}-celery]
-command=sh -c \'. {env_file} && {celery_path} -A {project_name}.celeryapp:app worker -B -E --loglevel=DEBUG --concurrency=10 -n worker1@%%h'
-directory={settings.BASE_DIR}
-user={user}
-numproc=4
-stdout_logfile=/var/log/{project_name}-celery.log
-stderr_logfile=/var/log/{project_name}-celery.log
-autostart=true
-autorestart=true
-startsecs=10
-stopwaitsecs=600
-"""
-
-        outfile_path = os.path.join(self.out_dir, f"{project_name}-celery.conf")
-
-        return self.write_file(outfile_path, file_content)
-
-    def write_celery_deploy(self, celery_conf):
-        deploy_path = os.path.join(self.out_dir, "deploy-celery.sh")
-        deploy_content = f"""#!/bin/bashtop_dir
-sudo ln -sf {celery_conf} /etc/supervisor/conf.d/{os.path.basename(celery_conf)}
-
-# Restart supervisor
-sudo pkill celery -f
-sudo supervisorctl reload
-"""
-        full_deploy_path = self.write_file(deploy_path, deploy_content)
-
-        return full_deploy_path
 
     def generate_uwsgi_ini(self):
         project_name = settings.WSGI_APPLICATION.split(".")[0]
@@ -394,37 +198,6 @@ cheaper-busyness-backlog-step = 2    ; How many emergency workers to create if t
 
         return file_content
 
-    def write_project_uwsgi_ini(self):
-        outfile_path = os.path.join(self.out_dir, "uwsgi.ini")
-
-        file_content = self.generate_uwsgi_ini()
-        return self.write_file(outfile_path, file_content)
-
-    def write_uwsgi_service(self, ini_file="<UPDATE WITH PATH TO .ini FILE>"):
-        uwsgi_path = os.path.join(os.path.dirname(sys.executable), "uwsgi")
-        user = self._resolve_var("USER", "username")
-        project_name = settings.WSGI_APPLICATION.split(".")[0]
-
-        file_content = f"""[Unit]
-Description={project_name} uWSGI daemon
-After=network.target
-
-[Service]
-User={user}
-Group=www-data
-Type=simple
-ExecStart=/bin/bash -c '{uwsgi_path} --ini {ini_file}'
-Restart=on-failure
-RestartSec=10s
-
-[Install]
-WantedBy=multi-user.target
-"""
-
-        outfile_path = os.path.join(self.out_dir, "uwsgi.service")
-
-        return self.write_file(outfile_path, file_content)
-
     def generate_uwsgi_service(self, ini_file_path: Path):
         user = self._resolve_var("USER", "username")
         project_name = settings.WSGI_APPLICATION.split(".")[0]
@@ -446,160 +219,6 @@ RestartSec=10s
 WantedBy=multi-user.target
 """
         return file_content
-
-    def write_uwsgi_service_deploy(self, service_file):
-        deploy_path = os.path.join(self.out_dir, "deploy-uwsgi-service.sh")
-        service_name = os.path.splitext(os.path.basename(service_file))[0]
-        deploy_content = f"""#!/bin/bash
-sudo ln -sf {service_file} /etc/systemd/system
-
-# refresh service
-sudo systemctl daemon-reload
-sudo systemctl stop {service_name}
-sudo pkill uwsgi -f
-sudo systemctl start {service_name}
-"""
-        full_deploy_path = self.write_file(deploy_path, deploy_content)
-
-        return full_deploy_path
-
-    def write_nginx_site_conf(self):
-        print("not fully implemented")
-        return
-
-        SITE_HOST_NAME = self._resolve_var("SITE_HOST_NAME")
-        file_name = SITE_HOST_NAME + ".conf"
-
-        MEDIA_ROOT = self._resolve_var("MEDIA_ROOT")
-        MEDIA_URL = self._resolve_var("MEDIA_URL")
-        MEDIA_URL_clean = MEDIA_URL.rstrip("/")
-
-        STATIC_ROOT = self._resolve_var("STATIC_ROOT")
-        STATIC_URL = self._resolve_var("STATIC_URL")
-        STATIC_URL_clean = STATIC_URL.rstrip("/")
-
-        LOCAL_ROOT = self._resolve_var("LOCAL_ROOT")
-        top_dir = os.path.dirname(LOCAL_ROOT)
-        project_name = os.path.basename(LOCAL_ROOT)
-
-        file_content = f"""# {file_name}
-
-# the upstream component nginx needs to connect to
-upstream {project_name} {{
-    server unix:{top_dir}/{project_name}.sock;
-}}
-
-# configuration of the server
-server {{
-    listen      80;
-    server_name {SITE_HOST_NAME};
-    charset     utf-8;
-
-    # max upload size
-    client_max_body_size 75M;   # adjust to taste
-
-    location {MEDIA_URL_clean}  {{
-        alias {MEDIA_ROOT};
-    }}
-
-    location {STATIC_URL_clean} {{
-        alias {STATIC_ROOT};
-    }}
-
-    location / {{
-        uwsgi_pass  {project_name};
-        include     /etc/nginx/uwsgi_params;
-    }}
-}}
-"""
-
-        outfile_path = os.path.join(self.out_dir, file_name)
-
-        return self.write_file(outfile_path, file_content)
-
-    def write_nginx_ssl_site_conf(self):
-        print("not fully implemented")
-        return
-
-        SITE_HOST_NAME = self._resolve_var("SITE_HOST_NAME")
-        file_name = SITE_HOST_NAME + "-ssl.conf"
-
-        MEDIA_ROOT = self._resolve_var("MEDIA_ROOT")
-        MEDIA_URL = self._resolve_var("MEDIA_URL")
-        MEDIA_URL_clean = MEDIA_URL.rstrip("/")
-
-        STATIC_ROOT = self._resolve_var("STATIC_ROOT")
-        STATIC_URL = self._resolve_var("STATIC_URL")
-        STATIC_URL_clean = STATIC_URL.rstrip("/")
-
-        LOCAL_ROOT = self._resolve_var("LOCAL_ROOT")
-        top_dir = os.path.dirname(LOCAL_ROOT)
-        project_name = os.path.basename(LOCAL_ROOT)
-
-        ## attempt to find LE cert for this domain
-        fullchain_path = f"/etc/letsencrypt/live/{SITE_HOST_NAME}/fullchain.pem"
-        privkey_path = f"/etc/letsencrypt/live/{SITE_HOST_NAME}/privkey.pem"
-        file_content = f"""# {file_name}
-
-# the upstream component nginx needs to connect to
-upstream {project_name} {{
-    server unix:{top_dir}/{project_name}.sock;
-}}
-
-server {{
-
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    server_name {SITE_HOST_NAME};
-
-    location / {{
-        uwsgi_pass  {project_name};
-        include     /etc/nginx/uwsgi_params;
-    }}
-
-    return 301 https://$host$request_uri;
-}}
-
-server {{
-    listen 443 ssl;
-    server_name {SITE_HOST_NAME};
-
-    charset     utf-8;
-
-    # max upload size
-    client_max_body_size 75M;   # adjust to taste
-
-    location {MEDIA_URL_clean}  {{
-        alias {MEDIA_ROOT};
-    }}
-
-    location {STATIC_URL_clean} {{
-        alias {STATIC_ROOT};
-    }}
-
-    location / {{
-        uwsgi_pass  {project_name};
-        include     /etc/nginx/uwsgi_params;
-    }}
-
-    location /geoserver {{
-        proxy_pass http://localhost:8080/geoserver;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_redirect off;
-    }}
-
-    ssl_certificate {fullchain_path};
-    ssl_certificate_key {privkey_path};
-    include /etc/letsencrypt/options-ssl-nginx.conf;
-    ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
-}}
-"""
-
-        outfile_path = os.path.join(self.out_dir, file_name)
-
-        return self.write_file(outfile_path, file_content)
 
     def generate_celery_service(self):
         log_dir = self._resolve_var("LOG_DIR", settings.LOG_DIR)
@@ -627,6 +246,32 @@ WantedBy=multi-user.target
 """
 
         return file_content
+
+    def create_services(self, destination: Path):
+        destination.mkdir(exist_ok=True)
+        cs_path = Path(destination, "celery.service")
+        self._write_file(self.generate_celery_service(), cs_path)
+
+        ui_path = Path(destination, "uwsgi.ini")
+        self._write_file(self.generate_uwsgi_ini(), ui_path)
+
+        us_path = Path(destination, "uwsgi.service")
+        self._write_file(self.generate_uwsgi_service(ui_path), us_path)
+
+        print(f"""services created. to deploy, run the following commands:
+
+# update celery 
+sudo ln -sf {cs_path.absolute()} /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl restart celery
+
+# update uwsgi
+sudo ln -sf {us_path.absolute()} /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl restart uwsgi
+""")
+
+        return [cs_path, ui_path, us_path]
 
     def generate_error_pages(self):
         rf = RequestFactory()
@@ -664,26 +309,3 @@ WantedBy=multi-user.target
             print(url)
             with open(Path(dest, url.split("/")[-1]), mode="wb") as file:
                 file.write(response.content)
-
-    def create_services(self):
-        self.out_dir = ".services"
-        celery_service_path = self._write_file(self.generate_celery_service(), "celery.service")
-        print(celery_service_path.absolute())
-
-        uwsgi_ini_path = self._write_file(self.generate_uwsgi_ini(), "uwsgi.ini")
-        uwsgi_service_path = self._write_file(
-            self.generate_uwsgi_service(uwsgi_ini_path), "uwsgi.service"
-        )
-
-        print(f"""services created. to deploy, run the following commands:
-
-# update celery 
-sudo ln -sf {celery_service_path.absolute()} /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl restart celery
-
-# update uwsgi
-sudo ln -sf {uwsgi_service_path.absolute()} /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl restart uwsgi
-""")
