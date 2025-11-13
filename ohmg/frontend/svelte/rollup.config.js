7292fa6a1345d25e2fae239854107b5a77c2e19d
Adam Cox <mr.adamcox@gmail.com>
2025-11-12 22:02:42 -0600

    don't serve individual bundle builds during dev

--- ohmg/frontend/svelte/rollup.config.js
+++ ohmg/frontend/svelte/rollup.config.js
@@ -7,27 +7,6 @@ import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
-function serve() {
-  let server;
-
-  function toExit() {
-    if (server) server.kill(0);
-  }
-
-  return {
-    writeBundle() {
-      if (server) return;
-      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
-        stdio: ['ignore', 'inherit', 'inherit'],
-        shell: true,
-      });
-
-      process.on('SIGTERM', toExit);
-      process.on('exit', toExit);
-    },
-  };
-}
-
 function componentExportDetails(componentName) {
   return {
     input: `./src/${componentName.toLowerCase()}.js`,
@@ -60,10 +39,6 @@ function componentExportDetails(componentName) {
         exportConditions: ['svelte'],
       }),
       commonjs(),
-
-      // In dev mode, call `npm run start` once
-      // the bundle has been generated
-      !production && serve(),
 
       // Watch the `public` directory and refresh the
       // browser on changes when not in production
