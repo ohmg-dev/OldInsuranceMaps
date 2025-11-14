65b773c42ce3e6d5358f3667aecab9c5d353d763
Adam Cox <mr.adamcox@gmail.com>
2025-11-14 15:58:10 -0600

    add published flag for partner model

--- ohmg/frontend/models.py
+++ ohmg/frontend/models.py
@@ -57,6 +57,7 @@ class Partner(models.Model):
     url = models.CharField(max_length=400, null=True, blank=True)
     description = MarkdownxField(null=True, blank=True)
     sortorder = models.IntegerField(null=True, blank=True)
+    published = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
