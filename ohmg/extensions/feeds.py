a9bec6a917cdfd4ef2cc73faabfa91621a7c8980
Josiah White <josiahwhite@gmail.com>
2026-02-18 20:50:19 -0800

    Creating complicated Q statement to get SessionBase

--- ohmg/extensions/feeds.py
+++ ohmg/extensions/feeds.py
@@ -1,20 +1,39 @@
 from django.contrib.syndication.views import Feed
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 
 from ohmg.places.models import Place
+from ohmg.georeference.models import SessionBase
+
+NUM_RSS_RETURNS = 100
 
 
 class PlaceFeed(Feed):
 
-    def get_object(self, request, place_slug: str) -> Place:
-        return get_object_or_404(Place, slug=place_slug)
+    def get_object(self, request, place: str) -> Place:
+        res = get_object_or_404(Place, slug=place)
+        return res
 
     def items(self, item: Place):
         # TODO: Ultimately return SessionBase for georef/prep
         #   Get map locales, then document, layer
         #   Return last X changes sorted by date_modified
-        maps = item.locales_set.all()
-        return []
+        q = (
+            # Checking this place directly
+            Q(doc2__map__locales__place=item)
+            | Q(reg2__document__map__locales__place=item)
+            | Q(lyr2__region__document__map__locales__place=item)
+            | Q(map__locales__place=item)
+            # Checking this place's parents now
+            | Q(doc2__map__locales__place__direct_parents=item)
+            | Q(reg2__document__map__locales__place__direct_parents=item)
+            | Q(lyr2__region__document__map__locales__place__direct_parents=item)
+            | Q(map__locales__plac__direct_parentse=item)
+        )
+        sessions = SessionBase.objects.filter(
+            q,
+        ).order_by("-date_modified")[:NUM_RSS_RETURNS]
+        return sessions
 
     def item_title(self, item: Place) -> str:
         # TODO: Not Place, more like SessionBase
