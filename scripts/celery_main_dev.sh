9af0cd02d9ca9a42bd7236290fff174b42d38226
Adam Cox <mr.adamcox@gmail.com>
2026-05-14 13:31:36 -0500

    split mosaic queue into new celery worker

---
+++ scripts/celery_main_dev.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+uv run celery -A ohmg.conf.celery:app worker \
+	-Q split,georeference,map,housekeeping \
+	--without-gossip \
+	--without-mingle \
+	-Ofair -B -E \
+	--statedb=worker.state \
+	-s celerybeat-schedule \
+	--loglevel=DEBUG \
+	--concurrency=10 \
+	-n worker1@%h
