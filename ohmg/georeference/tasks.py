31bc8d6c07d3ad0c2568e659b94514782971381d
Adam Cox <mr.adamcox@gmail.com>
2026-06-05 15:58:43 -0500

    implement scheduling for queued mosaic jobs; fix imports

--- ohmg/georeference/tasks.py
+++ ohmg/georeference/tasks.py
@@ -9,17 +9,13 @@ from ohmg.conf.celery import app
 from ohmg.core.models import LayerSet
 from ohmg.core.utils.s3 import get_boto3_s3_client
 
-from .models import (
-    GeorefSession,
-    PrepSession,
-)
-from .sessions import delete_expired_session_locks
-
 logger = logging.getLogger(__name__)
 
 
 @app.task
 def run_preparation_session(sessionid):
+    from .models import PrepSession
+
     session = PrepSession.objects.get(pk=sessionid)
     session.run()
     return session.pk
@@ -27,6 +23,8 @@ def run_preparation_session(sessionid):
 
 @app.task
 def bulk_run_preparation_sessions(sessionids):
+    from .models import PrepSession
+
     for sessionid in sessionids:
         session = PrepSession.objects.get(pk=sessionid)
         session.run()
@@ -35,6 +33,8 @@ def bulk_run_preparation_sessions(sessionids):
 
 @app.task
 def run_georeference_session(sessionid):
+    from .models import GeorefSession
+
     session = GeorefSession.objects.get(pk=sessionid)
     session.run()
     return session.pk
@@ -42,6 +42,8 @@ def run_georeference_session(sessionid):
 
 @app.task
 def delete_stale_sessions():
+    from .sessions import delete_expired_session_locks
+
     delete_expired_session_locks()
 
 
@@ -52,16 +54,26 @@ def delete_preview_vrts(id):
 
 
 @app.task
-def create_mosaic_cog(layersetid):
-    from ohmg.georeference.mosaicker import Mosaicker
+def create_mosaic_cog(layersetid, jobid=None):
+    from .models import Job
+    from .mosaicker import Mosaicker
 
+    logger.debug("begin create mosaic task")
     try:
         layerset = LayerSet.objects.get(pk=layersetid)
     except LayerSet.DoesNotExist:
         logger.warning(f"LayerSet does not exist: {layersetid}. Cancelling mosaic creation.")
-    m = Mosaicker()
-    m.generate_cog(layerset)
-    m.cleanup_files()
+    try:
+        m = Mosaicker()
+        success = True
+        message = m.generate_cog(layerset)
+        m.cleanup_files()
+    except Exception as e:
+        success = False
+        message = e
+
+    if jobid:
+        Job.objects.get(pk=jobid).end(success=success, message=message)
 
 
 @app.task
@@ -94,3 +106,21 @@ def cleanup_existing_tileset(prefix):
             )
         except FileNotFoundError:
             pass
+
+
+@app.task
+def run_queued_mosaic_jobs():
+    from ohmg.georeference.models import Job
+
+    max_jobs_ct = settings.MAX_CONCURRENT_MOSAIC_JOBS
+
+    mosaic_jobs = Job.objects.filter(operation__in=["layerset_to_cog", "layerset_to_xyz"])
+
+    ## if there are already max mosaic jobs running, don't start another one
+    running_jobs_ct = mosaic_jobs.filter(stage="running").count()
+    spots_left = max_jobs_ct - running_jobs_ct
+    if spots_left:
+        queued_jobs = mosaic_jobs.filter(stage="queued").order_by("date_queued")
+        for job in queued_jobs[:spots_left]:
+            logger.info(f"starting queued job {job.pk}: {job.operation}, {job.target}")
+            job.run()
