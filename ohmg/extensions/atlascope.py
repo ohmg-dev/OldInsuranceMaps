0a3047fd78e5a41b327fba3af272121e76510d7a
Adam Cox <mr.adamcox@gmail.com>
2026-08-18 15:32:14 -0500

    update footprints output for atlascope

--- ohmg/extensions/atlascope.py
+++ ohmg/extensions/atlascope.py
@@ -1,21 +1,24 @@
 import json
 
-from django.conf import settings
+import topojson
 from django.contrib.gis.geos import GEOSGeometry, MultiPolygon
 from ninja import Schema
 
 from ..core.models import LayerSet
+from ..places.models import Place
 
 
 def generate_atlascope_properties(layerset: LayerSet):
     return {
         "identifier": layerset.map.identifier,
-        "publisherShort": "Sanborn",
+        "publisherShort": layerset.map.publisher
+        if layerset.map.publisher
+        else layerset.map.creator,
         "year": layerset.map.year,
-        "bibliographicEntry": "_Richards standard atlas of the town of Greenfield_ (Richards Map Company, 1918)",
+        "bibliographicEntry": f"Fire Insurance Map of {layerset.map.title} (Sanborn Map Company)",
         "source": {
-            "type": "tilejson",
-            "url": f"{settings.SITEURL.rstrip('/')}/map/{layerset.map.identifier}/{layerset.category.slug}/tilejson",
+            "type": "xyz",
+            "url": f"{layerset.xyz_tiles_url}/{{z}}/{{x}}/{{y}}.png",
         },
         "catalogPermalink": f"https://loc.gov/item/{layerset.map.identifier}",
         "heldBy": ["Library of Congress"],
@@ -34,6 +37,32 @@ def generate_atlascope_geometry(layerset: LayerSet):
         return json.loads(geoms.unary_union.json)
     else:
         return {"type": "MultiPolygon", "coordinates": []}
+
+
+def generate_atlascope_footprints(place: Place, override_data_name: str = None):
+    maps = sorted(place.map_set.all().exclude(hidden=True), key=lambda x: x.year)
+
+    features = []
+    for map in maps:
+        ls = map.get_layerset("main-content")
+        if ls and ls.xyz_tiles_url:
+            feature = AtlascopeLayersetFeature.from_orm(ls).dict()
+            features.append(feature)
+
+    topo = topojson.Topology(
+        {
+            "type": "FeatureCollection",
+            "features": features,
+        }
+    )
+    topo_json = json.loads(topo.to_json())
+
+    ## allow override of the data property name in the topojson
+    if override_data_name:
+        topo_json["objects"][override_data_name] = topo_json["objects"]["data"]
+        del topo_json["objects"]["data"]
+
+    return topo_json
 
 
 class AtlascopeLayersetFeature(Schema):
